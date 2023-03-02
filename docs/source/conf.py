99b0e19050bd88040edbd1f26f4922c93374d919
John-Paul Navarro <navarro@anl.gov>
2023-03-02 09:12:38 -0600

    Very basic

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'readthedocs.access-ci.org'
+copyright = '2023, ACCESS'
+author = 'JP Navarro'
 
 release = '0.1'
 version = '0.1.0'
